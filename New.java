e282c807b701f8b1e3cac0f8849f732fc9459f29
dakyumlee <oicrcutie@gmail.com>
2023-04-09 16:37:02 +0900

    https://github.com/dakyumlee/calculator.git

---
+++ New.java
@@ -0,0 +1,27 @@
+package One;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a4 = 0;
+		a4 = multiplication(10,30);
+		System.out.println(a4);
+	}
+	public static int add(int a1, int a2) {
+		int a3 = a1 + a2;
+		return a3;
+	}
+	public static int minus(int a1, int a2) {
+		int a3 = a2 - a1;
+		return a3;
+	}
+	public static int division(int a1, int a2)	{
+		int a3 = a2 / a1;
+		return a3;
+	}
+	public static int multiplication(int a1, int a2) {
+		int a3 = a1 * a2;
+		return a3;
+	}
+}
